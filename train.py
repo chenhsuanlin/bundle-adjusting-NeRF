5b3300adac485da5b8662338db023e670efb337a
szymanowiczs <szymanowiczs@gmail.com>
2021-12-15 12:45:42 +0000

    Wrap train in main()

--- train.py
+++ train.py
@@ -6,22 +6,26 @@ import importlib
 import options
 from util import log
 
-log.process(os.getpid())
-log.title("[{}] (PyTorch code for training NeRF/BARF)".format(sys.argv[0]))
+def main():
+    log.process(os.getpid())
+    log.title("[{}] (PyTorch code for training NeRF/BARF)".format(sys.argv[0]))
 
-opt_cmd = options.parse_arguments(sys.argv[1:])
-opt = options.set(opt_cmd=opt_cmd)
-options.save_options_file(opt)
+    opt_cmd = options.parse_arguments(sys.argv[1:])
+    opt = options.set(opt_cmd=opt_cmd)
+    options.save_options_file(opt)
 
-with torch.cuda.device(opt.device):
+    with torch.cuda.device(opt.device):
 
-    model = importlib.import_module("model.{}".format(opt.model))
-    m = model.Model(opt)
+        model = importlib.import_module("model.{}".format(opt.model))
+        m = model.Model(opt)
 
-    m.load_dataset(opt)
-    m.build_networks(opt)
-    m.setup_optimizer(opt)
-    m.restore_checkpoint(opt)
-    m.setup_visualizer(opt)
+        m.load_dataset(opt)
+        m.build_networks(opt)
+        m.setup_optimizer(opt)
+        m.restore_checkpoint(opt)
+        m.setup_visualizer(opt)
 
-    m.train(opt)
+        m.train(opt)
+
+if __name__ == '__main__':
+    main()
